7e0b9c68560af8ace50a8c285659b421b1b9d9a6
Zeeshan Saleem <zeeshansaleem13@gmail.com>
2025-09-15 01:19:33 -0700

    add env

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-let API_BASE_URL = "https://web-production-8e48a.up.railway.app/";
+let API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 if (import.meta.env.MODE === "development") {
   API_BASE_URL = "http://127.0.0.1:8000";
