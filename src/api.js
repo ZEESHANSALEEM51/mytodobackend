1560e98456ca968ce367b513b2d2ae89f3f817c1
Zeeshan Saleem <zeeshansaleem13@gmail.com>
2025-09-15 01:32:07 -0700

    new changes

--- src/api.js
+++ src/api.js
@@ -1,8 +1,17 @@
 import axios from "axios";
-import API_BASE_URL from "./config.js";   // <-- extension bhi add kar do
+import API_BASE_URL from "./config.js";   // extension theek hai
 
 const api = axios.create({
   baseURL: API_BASE_URL,
+});
+
+// 🔑 Token har request ke sath attach karne ke liye interceptor
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
